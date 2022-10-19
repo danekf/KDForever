fbe7d4952b2680ee3123074419a1ef593bfb08c4
Danek <danek.fill@outlook.com>
2022-10-19 15:12:36 -0400

    menu bar refinment

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,14 +6,18 @@ import { ContactUsComponent } from './contact-us/contact-us.component';
 import { DirectionsComponent } from './directions/directions.component';
 
 const routes: Routes = [
+  // the main paths
+  {path:'home', component: WelcomePageComponent},
+  {path:'contact-us', component: ContactUsComponent},
+  {path:'directions', component: DirectionsComponent},
+
   //testing component path, change component and whenever you go to /test that page will render. Makes things a bit easier if testing single component
   {path:'test', component: MenuComponent},
-  //default router component
-  {path:'', component: WelcomePageComponent},
 
-  // the rest
-  {path:'contact-us', component: ContactUsComponent},
-  {path:'directions', component: DirectionsComponent}
+  //Redirects and wildcard catches
+  {path:'', redirectTo: '/home', pathMatch: 'full'},
+  {path:'**', redirectTo: '/home', pathMatch: 'full'},
+
 ];
 
 @NgModule({
