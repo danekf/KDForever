576260967a9622937ec63e3660ac815c717a86b0
Danek <danek.fill@outlook.com>
2022-12-19 15:01:26 -0500

    minor adjustments to menu wording

--- src/app/contact-us/contact-us.component.ts
+++ src/app/contact-us/contact-us.component.ts
@@ -13,7 +13,7 @@ export class ContactUsComponent implements OnInit {
   //create FAQs to be displayed using FAQ type declared above
   faqs: FAQ = [
     {question:'What is the date of the wedding?', answer:'The wedding is on September 2nd 2023'},
-    {question:'What is question #2?', answer:'This is question'}
+    {question: 'How can I contact you if I have additional questions?', answer: 'You can reach out to us via email at KD.Together@outlook.com'}
   ]
 
   constructor() {
